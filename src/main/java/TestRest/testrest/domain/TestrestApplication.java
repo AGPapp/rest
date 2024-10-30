8b2b357d2daf1cff83265a99162ee4655cd1fa98
Andrey <agpopov@appline.ru>
2024-10-30 12:45:24 +0300

    комит

---
+++ src/main/java/TestRest/testrest/domain/TestrestApplication.java
@@ -0,0 +1,20 @@
+package TestRest.testrest.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestrestApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String index(){
+		return "Привет!";
+	}
+}
---
+++ src/test/java/TestRest/testrest/TestrestApplicationTests.java
@@ -0,0 +1,13 @@
+package TestRest.testrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
